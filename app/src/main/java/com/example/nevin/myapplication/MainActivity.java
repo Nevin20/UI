3ac1b3f81554d60e86544bcaf55390a2c1503046
Nevin20 <nevinrocky6@gmail.com>
2018-08-29 21:35:52 +0530

    first commit

---
+++ app/src/main/java/com/example/nevin/myapplication/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.nevin.myapplication;
+
+        import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener
+                (new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment selectedFragment = null;
+                        switch (item.getItemId()) {
+                            case R.id.navigation_store:
+                                selectedFragment = ItemOneFragment.newInstance();
+                                break;
+                            case R.id.navigation_delivery:
+                                selectedFragment = ItemTwoFragment.newInstance();
+                                break;
+                            case R.id.navigation_plans:
+                                selectedFragment = ItemThreeFragment.newInstance();
+                                break;
+                            case R.id.navigation_profile:
+                                selectedFragment = ItemFourFragment.newInstance();
+                                break;
+                        }
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame_container, selectedFragment);
+                        transaction.commit();
+                        return true;
+                    }
+                });
+
+        //Manually displaying the first fragment - one time only
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, ItemOneFragment.newInstance());
+        transaction.commit();
+
+        //Used to select an item programmatically
+        //bottomNavigationView.getMenu().getItem(2).setChecked(true);
+    }
+}
